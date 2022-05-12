e5c5a60e20db758abefdd239cea17c10790205e9
Dat0309 <anhtheboy011@gmail.com>
2022-05-12 21:26:42 +0700

    Update splash screen

---
+++ doctor_appointment/lib/constant/constant.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color? primaryColor = const Color(0xff75CFB8);
+Color? primaryBgColor = const Color(0xffBBDFC8);
+Color? secondaryColor = const Color(0xffF0E5D8);
+Color? thirthColor = const Color(0xffFFC478);
--- doctor_appointment/lib/views/splash_screen.dart
+++ doctor_appointment/lib/views/splash_screen.dart
@@ -1,37 +1,24 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:doctor_appointment/constant/constant.dart';
+import 'package:doctor_appointment/views/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:page_transition/page_transition.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Container(
-        alignment: Alignment.center,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 300,
-              width: 300,
-              child: Lottie.asset('assets/images/doctorsplash.json'),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              'Sức khoẻ thông minh',
-              style: TextStyle(
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ],
-        ),
-      ),
+    Size size = MediaQuery.of(context).size;
+    return AnimatedSplashScreen(
+      duration: 4000,
+      backgroundColor: primaryColor!,
+      splash: Lottie.asset('assets/images/doctorsplash.json'),
+      nextScreen: const LoginScreen(),
+      splashIconSize: size.height * 0.4,
+      splashTransition: SplashTransition.sizeTransition,
+      pageTransitionType: PageTransitionType.rightToLeft,
     );
   }
 }
