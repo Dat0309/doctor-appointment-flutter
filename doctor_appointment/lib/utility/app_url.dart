d174b87c59bccac663a8e018f7d7f579b9f2fe0f
Dat0309 <anhtheboy011@gmail.com>
2022-05-12 13:47:58 +0700

    Update

---
+++ doctor_appointment/lib/utility/app_url.dart
@@ -0,0 +1,6 @@
+class AppUrl {
+  static const String baseUrl = 'https://doctorapiv1.herokuapp.com/api';
+  static const String login = baseUrl + '/login';
+  static const String register = baseUrl + '/register';
+  static const String user = baseUrl + '/users/';
+}
